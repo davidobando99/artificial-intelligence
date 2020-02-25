ee9673d8c51c13146b3c7076fc0c97c6ca902fc3
davidobando99 <da.vi.d99@hotmail.com>
2020-02-25 17:12:02 -0500

    machine turn solucionado

--- tic-tac-toe/tic_tac_toe.py
+++ tic-tac-toe/tic_tac_toe.py
@@ -1,5 +1,5 @@
 import re
-import random
+import random as r
 
 _PLAYER = "player"
 _MACHINE = "machine"
@@ -18,18 +18,19 @@ class TicTacToeGame():
   
   def is_over(self): # TODO: Finish this function by adding checks for a winning game (rows, columns, diagonals)
  
-    win= ((self.board[6] == self.board[7] ==  self.board[8]) or 
-    (self.board[3] == self.board[4] ==  self.board[5]) or 
-    (self.board[0] ==  self.board[1] ==  self.board[2] ) or 
-    (self.board[6] ==  self.board[3] ==  self.board[0] ) or 
-    (self.board[7] ==  self.board[4] ==  self.board[1] ) or 
-    (self.board[8] ==  self.board[5] ==  self.board[2] ) or 
-    (self.board[6] ==  self.board[4] ==  self.board[2] ) or 
-    (self.board[8] ==  self.board[4] ==  self.board[0] ))
+    if(self.turn== _PLAYER): symbol = _MACHINE_SYMBOL
+    elif(self.turn== _MACHINE): symbol = _PLAYER_SYMBOL    
+    return((self.board[6] == symbol and self.board[7] == symbol and self.board[8] == symbol) or 
+    (self.board[3] == symbol and self.board[4] == symbol and self.board[5] == symbol) or 
+    (self.board[0] == symbol and self.board[1] == symbol and self.board[2] == symbol) or 
+    (self.board[6] == symbol and self.board[3] == symbol and self.board[0] == symbol) or 
+    (self.board[7] == symbol and self.board[4] == symbol and self.board[1] == symbol) or 
+    (self.board[8] == symbol and self.board[5] == symbol and self.board[2] == symbol) or 
+    (self.board[6] == symbol and self.board[4] == symbol and self.board[2] == symbol) or 
+    (self.board[8] == symbol and self.board[4] == symbol and self.board[0] == symbol))
 
-    if(self.turn== _MACHINE and win): self.winner == _PLAYER
-    elif(self.turn== _PLAYER and win): self.winner == _MACHINE 
-  
+
+    
    
   def play(self):
     if self.turn == _PLAYER:
@@ -38,6 +39,8 @@ class TicTacToeGame():
     else:
       self.machine_turn()
       self.turn = _PLAYER
+
+   
     
 
   def player_choose_cell(self):
@@ -66,7 +69,7 @@ class TicTacToeGame():
     self.board[chosen_cell] = _PLAYER_SYMBOL
 
   def machine_turn(self): # TODO: Finish this function by making the machine choose a random cell (use random module)
-      ran = random.randrange(0,8)
+      ran = r.randrange(0,8)
         
       if self.board[ran] is  None:
         self.board[ran] = _MACHINE_SYMBOL
@@ -87,8 +90,11 @@ class TicTacToeGame():
     print(self.format_board())
     print()
 
+  
+
   def print_result(self): # TODO: Finish this function in order to print the result based on the *winner*
-    self.is_over()
-    if self.winner==_MACHINE: print("the winner is MACHINE")
-    elif self.winner== _PLAYER: print("the winner is PLAYER")
+    if(self.turn == _MACHINE and self.is_over()): print("the winner is PLAYER")
+    elif(self.turn== _PLAYER and self.is_over()): print("the winner is MACHINE")
+    
+  
     
